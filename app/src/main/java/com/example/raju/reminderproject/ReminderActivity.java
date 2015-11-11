07e7dd24156024f78b272ae8f76ea7681038f4e3
gurpalrattu <gurpal_rattu@hotmail.com>
2015-11-10 23:19:33 -0800

    Changing the action bar menu items, created 2 new items for the list. New Reminder and Exit

--- app/src/main/java/com/example/raju/reminderproject/ReminderActivity.java
+++ app/src/main/java/com/example/raju/reminderproject/ReminderActivity.java
@@ -36,14 +36,7 @@ public class ReminderActivity extends AppCompatActivity {
 
         mListView.setAdapter(arrayAdapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
